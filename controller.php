029274bc6fb8b2d4079b90c7a5b954ba59c29604
francesco <francesco.petrone@hotmail.com>
2026-06-25 12:29:40 +0200

    1.2.0-rc6:  release candidate
    (cherry picked from commit d7763bd8914f202d4a62b7419c0aa746dadbb01f)

--- controller.php
+++ controller.php
@@ -30,7 +30,7 @@ class Controller extends Package
 
     protected $appVersionRequired = '9.2.0';
 
-    protected $pkgVersion = '1.2.0-rc5';
+    protected $pkgVersion = '1.2.0-rc6';
 
     public function getPackageName()
     {
