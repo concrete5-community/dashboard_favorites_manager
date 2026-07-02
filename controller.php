98652a409adb55fc155b30ec03c6a20b1374a6d4
francesco <francesco.petrone@hotmail.com>
2026-07-02 08:01:02 +0200

    1.2.0-rc9: release candidate

--- controller.php
+++ controller.php
@@ -30,7 +30,7 @@ class Controller extends Package
 
     protected $appVersionRequired = '9.2.0';
 
-    protected $pkgVersion = '1.2.0-rc8';
+    protected $pkgVersion = '1.2.0-rc9';
 
     public function getPackageName()
     {
